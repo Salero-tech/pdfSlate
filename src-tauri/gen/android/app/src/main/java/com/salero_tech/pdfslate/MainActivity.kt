6e0f15b1cac722afac1c1fb96d855c904b2ba8e7
bollija3 <bollija3@students.zhaw.ch>
2025-10-30 21:13:41 +0100

    hide controls

--- src-tauri/gen/android/app/src/main/java/com/salero_tech/pdfslate/MainActivity.kt
+++ src-tauri/gen/android/app/src/main/java/com/salero_tech/pdfslate/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.salero_tech.pdfslate
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
+import android.view.WindowManager
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
+import android.graphics.Color
 
 class MainActivity : TauriActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
-    enableEdgeToEdge()
     super.onCreate(savedInstanceState)
+    
+    // Set black background for the window
+    window.decorView.setBackgroundColor(Color.BLACK)
+    
+    // Set black background for status bar and navigation bar
+    window.statusBarColor = Color.BLACK
+    window.navigationBarColor = Color.BLACK
+    
+    // Enable fullscreen mode - hide both status bar and navigation bar
+    val windowInsetsController = WindowCompat.getInsetsController(window, window.decorView)
+    windowInsetsController?.apply {
+      // Hide both status bar (top) and navigation bar (bottom)
+      hide(WindowInsetsCompat.Type.statusBars())
+      hide(WindowInsetsCompat.Type.navigationBars())
+      
+      // User can swipe to temporarily show bars
+      systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+
+    }
   }
 }
